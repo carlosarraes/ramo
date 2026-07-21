6c38f0bc93703c3bde23627b642f9cdaa9e66578
carraes <carraeshb@gmail.com>
2026-07-21 10:47:09 -0300

    style: format config exports

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,7 +2,7 @@ mod load;
 mod model;
 mod save;
 
-pub use load::{ConfigError, ConfigPaths, ConfigResolver};
 pub(crate) use load::user_config_dir;
+pub use load::{ConfigError, ConfigPaths, ConfigResolver};
 pub use model::{ConfigLayer, CustomThemeConfig, ResolvedConfig, ViewPreferences};
 pub use save::{ConfigSaveError, ViewPreferenceChanges, save_view_preferences};
