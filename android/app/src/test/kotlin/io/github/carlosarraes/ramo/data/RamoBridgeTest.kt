96b8adc8231dbf1824dbb77b8194cdabb974a5d0
carraes <carraeshb@gmail.com>
2026-08-15 02:37:02 -0300

    chore: release version 0.0.19

--- android/app/src/test/kotlin/io/github/carlosarraes/ramo/data/RamoBridgeTest.kt
+++ android/app/src/test/kotlin/io/github/carlosarraes/ramo/data/RamoBridgeTest.kt
@@ -8,6 +8,6 @@ class RamoBridgeTest {
     @Test
     fun reportsCoreVersion() = runTest {
         val bridge = NativeRamoBridge()
-        assertEquals("0.0.18", bridge.coreVersion())
+        assertEquals("0.0.19", bridge.coreVersion())
     }
 }
